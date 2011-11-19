30b5d0e7238275be30db14ce311fde8584107581
CCC <ccc.larc@gmail.com>
2011-11-19 14:32:46 +0800

    [GUI] Add view control

--- server_loading.py
+++ server_loading.py
@@ -115,50 +115,62 @@ def main():
     servers = [Server('A'), Server('B'), Server('C')]
     
     app = QtGui.QApplication(sys.argv)
-    win = QtGui.QSplitter()
-    
     model = ServerModel(servers)
-    view1 = QtGui.QListView()
-    view1.setModel(model)
-    win.addWidget(view1)
     
-    delegate = ServerProgressBarDelegate()
-    view2 = QtGui.QListView()
-    view2.setModel(model)
-    view2.setItemDelegate(delegate)
-    win.addWidget(view2)
+    view_control = [True, False, False, False]
+    show_view1 = view_control[0]
+    show_view2 = show_view1 & view_control[1]
+    show_view3 = show_view2 & view_control[2]
+    show_view4 = show_view3 & view_control[3]
     
-    load_sort_proxy_model = ServerLoadingSortProxyModel()
-    load_sort_proxy_model.setSourceModel(model)
-    load_sort_proxy_model.setDynamicSortFilter(True)
-    load_sort_proxy_model.sort(0, QtCore.Qt.DescendingOrder)
-    view3 = QtGui.QListView()
-    view3.setModel(load_sort_proxy_model)
-    view3.setItemDelegate(delegate)
-    win.addWidget(view3)
+    if show_view1:
+        win = QtGui.QSplitter()
+        
+        view1 = QtGui.QListView()
+        view1.setModel(model)
+        win.addWidget(view1)
     
-    load_filter_proxy_model = ServerLoadingFilterProxyModel()
-    load_filter_proxy_model.setSourceModel(load_sort_proxy_model)
-    view4 = QtGui.QListView()
-    view4.setModel(load_filter_proxy_model)
-    view4.setItemDelegate(delegate)
+    if show_view2:
+        delegate = ServerProgressBarDelegate()
+        view2 = QtGui.QListView()
+        view2.setModel(model)
+        view2.setItemDelegate(delegate)
+        win.addWidget(view2)
     
-    slider = QtGui.QSlider(QtCore.Qt.Horizontal)
-    slider.setRange(0, 100)
-    slider.setTickInterval(10)
-    slider.setTickPosition(QtGui.QSlider.TicksAbove)
-    slider.setValue(load_filter_proxy_model.lowBound())
-    slider.valueChanged.connect(load_filter_proxy_model.setLowBound)
+    if show_view3:
+        load_sort_proxy_model = ServerLoadingSortProxyModel()
+        load_sort_proxy_model.setSourceModel(model)
+        load_sort_proxy_model.setDynamicSortFilter(True)
+        load_sort_proxy_model.sort(0, QtCore.Qt.DescendingOrder)
+        view3 = QtGui.QListView()
+        view3.setModel(load_sort_proxy_model)
+        view3.setItemDelegate(delegate)
+        win.addWidget(view3)
     
-    layout4 = QtGui.QVBoxLayout()
-    layout4.addWidget(view4)
-    layout4.addWidget(slider)
+    if show_view4:
+        load_filter_proxy_model = ServerLoadingFilterProxyModel()
+        load_filter_proxy_model.setSourceModel(load_sort_proxy_model)
+        view4 = QtGui.QListView()
+        view4.setModel(load_filter_proxy_model)
+        view4.setItemDelegate(delegate)
+        
+        slider = QtGui.QSlider(QtCore.Qt.Horizontal)
+        slider.setRange(0, 100)
+        slider.setTickInterval(10)
+        slider.setTickPosition(QtGui.QSlider.TicksAbove)
+        slider.setValue(load_filter_proxy_model.lowBound())
+        slider.valueChanged.connect(load_filter_proxy_model.setLowBound)
+        
+        layout4 = QtGui.QVBoxLayout()
+        layout4.addWidget(view4)
+        layout4.addWidget(slider)
+        
+        widget4 = QtGui.QWidget()
+        widget4.setLayout(layout4)
+        win.addWidget(widget4)
     
-    widget4 = QtGui.QWidget()
-    widget4.setLayout(layout4)
-    win.addWidget(widget4)
-    
-    win.show()
+    if show_view1:
+        win.show()
     
     def _update_servers():
         for serv in servers:
